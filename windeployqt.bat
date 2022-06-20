4607e7c2d36518ba7dc54f8039a773be98005e8f
zhaitianbao <1257147469@qq.com>
2022-06-20 13:28:49 +0800

    更新打包工具

--- windeployqt.bat
+++ windeployqt.bat
@@ -2,20 +2,11 @@
 
 md %USERPROFILE%\Desktop\Test
 
-<<<<<<< HEAD
 copy ..\build-ImageProcessZTB-Desktop_Qt_5_12_8_MSVC2017_64bit-Release\ImageProcessZTB.exe %USERPROFILE%\Desktop\Test\
-
-xcopy .\64_release\* %USERPROFILE%\Desktop\Test\ /s /e /c /y /h /r
-
-cd /d %USERPROFILE%\Desktop\Test
-
-windeployqt ImageProcessZTB.exe --qmldir F:\qt5.12\QT\5.12.8\msvc2017_64\qml
-=======
-copy ..\build-ImageProcessZTB-Desktop_Qt_5_12_9_MSVC2017_64bit-Release\ImageProcessZTB.exe %USERPROFILE%\Desktop\Test\
 
 xcopy .\64_release\ %USERPROFILE%\Desktop\Test\ /s /e /c /y /h /r
 
 cd /d %USERPROFILE%\Desktop\Test
 
-windeployqt ImageProcessZTB.exe --qmldir D:\Qt\5.12.9\msvc2017_64\qml
->>>>>>> d528f27294b42d2b72cc81f258c0c18b721909d2
+windeployqt ImageProcessZTB.exe --qmldir C:\Qt\Qt5.12.8\5.12.8\msvc2017_64\qml
+
